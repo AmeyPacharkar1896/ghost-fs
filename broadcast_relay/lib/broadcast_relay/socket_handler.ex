cb07c04314c8b9a9fe476fdaba3b0f2dc4935602
Amey Pacharkar <ameypacharkar076@gmail.com>
2026-02-19 21:44:19 +0530

    fix: solved ghost bit transfer error

--- broadcast_relay/lib/broadcast_relay/socket_handler.ex
+++ broadcast_relay/lib/broadcast_relay/socket_handler.ex
@@ -2,22 +2,30 @@ defmodule BroadcastRelay.SocketHandler do
   @behaviour :cowboy_websocket
 
   def init(request, _state) do
+    IO.puts("🔌 Frontend connected to Socket!")
     {:cowboy_websocket, request, %{}}
   end
 
   def websocket_init(state) do
     Phoenix.PubSub.subscribe(BroadcastRelay.PubSub, "video_stream")
+    IO.puts("✅ Subscribed to 'video_stream' topic.")
     {:ok, state}
   end
 
   def websocket_info({:video_chunk, data}, state) do
+    IO.puts("📦 Socket received #{byte_size(data)} bytes! Pushing to browser...")
     {:reply, {:binary, data}, state}
   end
 
-  def websocket_info(_info, state) do
+  def websocket_info(info, state) do
+    IO.inspect(info, label: "⚠️ Unknown message received")
     {:ok, state}
   end
 
   def websocket_handle(_frame, state), do: {:ok, state}
-  def terminate(_reason, _req, _state), do: :ok
+
+  def terminate(reason, _req, _state) do
+    IO.puts("❌ Socket closed! Reason: #{inspect(reason)}")
+    :ok
+  end
 end
